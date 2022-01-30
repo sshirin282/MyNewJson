6b3b9927d61fc77aa6868b5a1999a07e9e8ed009
lenovo <sshirindrd@gmail.com>
2022-01-30 17:41:13 +0530

    lifeCycle

--- app/src/main/java/com/example/mynewjson/MainActivity.kt
+++ app/src/main/java/com/example/mynewjson/MainActivity.kt
@@ -9,15 +9,16 @@ import android.widget.Toolbar
 class MainActivity : AppCompatActivity() {
     lateinit var button: Button
     lateinit var button1: Button
-    lateinit var toolbar: Toolbar
+    //lateinit var toolbar: androidx.appcompat.widget.Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         button=findViewById(R.id.buttonActivity)
         button1=findViewById(R.id.buttonActivity1)
-        toolbar=findViewById(R.id.toolbar)
+//        toolbar=findViewById(R.id.tool1)
 
-        toolbar.setTitle("Practice")
+//        toolbar.setTitle("Practice")
+//        setSupportActionBar(toolbar)
 
         button.setOnClickListener {
             val intent=Intent(this,MainActivity2::class.java)
