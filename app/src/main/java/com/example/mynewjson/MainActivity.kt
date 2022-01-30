567177c88a7e9fd1351d77b5f82c6a7f58484283
lenovo <sshirindrd@gmail.com>
2022-01-30 17:19:42 +0530

    lifeCycle

--- app/src/main/java/com/example/mynewjson/MainActivity.kt
+++ app/src/main/java/com/example/mynewjson/MainActivity.kt
@@ -4,16 +4,27 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     lateinit var button: Button
+    lateinit var button1: Button
+    lateinit var toolbar: Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         button=findViewById(R.id.buttonActivity)
+        button1=findViewById(R.id.buttonActivity1)
+        toolbar=findViewById(R.id.toolbar)
+
+        toolbar.setTitle("Practice")
 
         button.setOnClickListener {
             val intent=Intent(this,MainActivity2::class.java)
+            startActivity(intent)
+        }
+        button1.setOnClickListener {
+            val intent=Intent(this,MainActivity3::class.java)
             startActivity(intent)
         }
     }
---
+++ app/src/main/java/com/example/mynewjson/MainActivity3.kt
@@ -0,0 +1,43 @@
+package com.example.mynewjson
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main3)
+        Toast.makeText(this,"OnCreate",Toast.LENGTH_LONG).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this,"onStart",Toast.LENGTH_LONG).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this,"onResume",Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this,"onPause",Toast.LENGTH_LONG).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this,"onStop",Toast.LENGTH_LONG).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this,"onRestart",Toast.LENGTH_LONG).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this,"onDestroy",Toast.LENGTH_LONG).show()
+    }
+}
